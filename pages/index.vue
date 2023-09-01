111f9a6353aedac5b45b532ea984f719d36a575b
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-01 18:57:32 +0100

    add pages

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Home</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci alias aliquid asperiores autem
+    cupiditate distinctio dolores doloribus eius est maiores modi necessitatibus nostrum, quidem sed similique totam
+    velit veniam.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto aut debitis doloremque doloribus
+    velit voluptate.
+  </p>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
